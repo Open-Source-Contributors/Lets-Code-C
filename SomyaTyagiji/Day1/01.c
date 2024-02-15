edfdb4569e1d2a82b7dc0fad00e978145c220691
Somya Tyagi <tyagisomya892@gmail.com>
2024-02-15 22:14:42 +0530

    I have complete my assignment

---
+++ SomyaTyagiji/Day1/01.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{ 
+    char name[50];
+    printf("what is your name? \n");
+    scanf("%s", name);
+    printf("welcome %s\n", name);
+  
+  char answer2[200]; 
+  printf("why do you want to learn c programming? \n");
+    scanf("%s", answer2);
+    printf("thank you for your answer %s\n", answer2);
+
+    char answer3[200];
+    printf("As an aspiring developer, what inspired you to pursue programming?\n");
+    scanf("%s", answer3);
+    printf("Nice answer keep it up %s\n", answer3);
+
+    char answer4[200];
+    printf("If you could create any software or application, what would it be and why?\n");
+    scanf("%s", answer4);
+    printf("Great answer %s\n", answer4);
+     
+     char answer5[200];
+    printf("As you embark on your programming journey, what positive impact do you hope to make in the tech community or beyond?\n");
+    scanf("%s", answer5);
+    printf("Good  answer %s\n", answer5);
+
+
+    return 0;
+
+    
+
+}
