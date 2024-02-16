c3e7f094ab406489f950e768a9678df98a2b9408
mahak121 <mahakpal2406@gmail.com>
2024-02-16 15:10:29 -0300

    I have complete my assignment

---
+++ MahakPal/Day1/01.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    char name[50];
+    printf("Hellow! What's your name?");
+    scanf("%s", name);
+    printf("welcome,%s!\n", name);
+    // Answer: MahakPal // 
+    
+    char solution2[200];
+    //Problem second//
+    printf("Why do you want to learn C?\n ");
+    scanf("%s", solution2);
+    printf("nice, keep it up,%s\n", solution2);
+    // Answer2: C is  most commonly used for system programming and many existing projects written in c //
+
+    char solution3[200];
+    //Problem third//
+    printf("As an inspiring developer, what inspired you to pursue programming?\n ");
+    scanf("%s", solution3);
+    printf("best of your luck,%s\n", solution3);
+    // Answer3: I like of problem solving and it has great job opportunities //
+
+
+    char solution4[200];
+    // Problem forth //
+    printf("If you could to create any software or application, what would it be and why?\n");
+    scanf("%s", solution4);
+    printf("good keep success,%s\n", solution4);
+    // Answer4: I haven't thought about it yet //
+
+
+    char solution5[200];
+    // Prolem fifth //
+    printf("As you embark on your programming journey, what positive impact do you hope to make in the tech community or beyond?\n ");
+    scanf("%s", solution5);
+    printf("good,keep it up,%s\n", solution5);
+    // Answer5: I haven't thought about it yet //
+
+    return 0;
+
+}
---
+++ MahakPal/Day1/o1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    // first question //
+    printf("Hello! What's your name? ");
+    scanf("%s\n", name);
+
+    printf("welcome, %s!\n", name);
+
+    // second question //
+
+    printf("Why do you want to learn C? ");
+
+    // third question //
+
+    printf("As an aspiring developer, what inspired you to pursue progrmming? ");
+
+    // fourth question //
+
+    printf("If you could create any software or application, what would it be and why? ");
+
+    // fifth question //
+
+    printf("As you embark on your programming journey, what positive impact do you hope to make in the tech community or beyond? ");
+
+    return 0;
+}
