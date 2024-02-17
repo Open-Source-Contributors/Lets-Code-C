4e096213e8bfee3f42ffcaa807c0f5ae2f682d02
Rishikesh Samant <146345711+rishikeshsamant@users.noreply.github.com>
2024-02-18 00:17:47 +0530

    Update 01.c

--- SparshKotiya/Day01/01.c
+++ SparshKotiya/Day01/01.c
@@ -49,11 +49,11 @@ int main()
 Scores:
 1. Successfully Submitted: 5/5
 
-2. Correctness of the Code: 3/5
+2. Correctness of the Code: 1/5
   --Comment: %s is only used for one string.
 
-3. Coding Standards Adherence: 4/5
+3. Coding Standards Adherence: 5/5
   --Comment: Consider improving code output cleanliness for enhanced readability.
 
-Total Score: 12/15
+Total Score: 14/15
 */
