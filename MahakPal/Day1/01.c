bd7df06fa117d0f2e1aa3ad997595b9f790ff639
Rishikesh Samant <146345711+rishikeshsamant@users.noreply.github.com>
2024-02-17 17:28:47 +0530

    Update 01.c

--- MahakPal/Day1/01.c
+++ MahakPal/Day1/01.c
@@ -41,3 +41,15 @@ int main()
     return 0;
 
 }
+
+/*
+Scores:
+1. Successfully Submitted: 5/5
+
+2. Correctness of the Code: 5/5
+
+3. Coding Standards Adherence: 4/5
+   - Comment: Consider improving code output cleanliness for enhanced readability.
+   
+Total Score: 14/15
+*/
