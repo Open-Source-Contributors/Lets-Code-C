74545e03de874fa018bb2abbd9c745b6e0e1a491
mahak121 <mahakpal2406@gmail.com>
2024-02-18 20:39:45 -0300

    Day2 assignment complete

---
+++ MahakPal/Day2/02.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //Input 5  ---->prime number //
+
+    int number,i, count=0;
+
+    printf("Enter any number\n");
+    scanf("%d", &number);
+    for (i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            count++;
+        }
+    }
+
+
+    if (count == 2)
+    {
+        printf("%d is a prime number\n", number);
+    }
+    else{
+        printf("%d is not a prime number\n", number);
+    }
+      
+    return 0;
+}
