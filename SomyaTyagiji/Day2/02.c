f0961d04c8871693adffa48f484fcbcc91bf78db
Somya Tyagi <tyagisomya892@gmail.com>
2024-02-18 19:00:00 +0530

    I have completed my assignment

---
+++ SomyaTyagiji/Day2/02.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int x;
+printf("enter the number \n");
+scanf("%d", &x);
+if (x%2==0)
+{
+    printf("the number is even \n");
+}
+else
+{
+    printf("the number is odd \n");
+    return 0;
+
+}
+}
+
