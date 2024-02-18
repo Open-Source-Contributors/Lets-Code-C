bd10be908a62662a975bdba73d3394a14e05f728
Somya Tyagi <tyagisomya892@gmail.com>
2024-02-18 22:39:37 +0530

    I have done my task

--- SomyaTyagiji/Day2/02.c
+++ SomyaTyagiji/Day2/02.c
@@ -1,18 +1,32 @@
-#include<stdio.h>
-int main ()
-{
-    int x;
-printf("enter the number \n");
-scanf("%d", &x);
-if (x%2==0)
-{
-    printf("the number is even \n");
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)// it is not a prime number.
+     {
+        return 0;
+    }
+    
+    for (int i = 2; i * i <= num; i++) 
+    {
+        if (num % i == 0)// it is  not a prime number.
+         {
+            return 0;
+        }
+    }
+    
+    return 1;
 }
-else
-{
-    printf("the number is odd \n");
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+    
     return 0;
-
 }
-}
-
