1083a082ffaa5623f2a5d23680786d201b2f72a4
Rishikesh Samant <146345711+rishikeshsamant@users.noreply.github.com>
2024-02-19 13:43:54 +0530

    Update 02.c

--- Solutions/Day2/02.c
+++ Solutions/Day2/02.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    int num;
-    printf("Enter a non-zero positive integer number: ");
-    scanf("%d",&num);
-
+int prime(int num) {
     //check num is positive number
     if (num <= 0) 
     return 1; //stop program execution
@@ -24,6 +20,19 @@ int main() {
         printf("%d is prime number.",num);
         else
         printf("%d is not prime number.",num);
+    }
+
+    return 0;
+
+}
+int main() {
+    int num;
+    printf("Enter a non-zero positive integer number: ");
+    scanf("%d",&num);
+
+    int invalid = prime(num);
+    if (invalid) {
+        printf("Invalid Input.");
     }
 
     return 0;
