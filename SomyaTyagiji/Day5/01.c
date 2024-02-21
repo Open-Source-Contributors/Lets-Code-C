56746a6871c7b4d011bc4a01d2ff047b9034de1a
Somya Tyagi <tyagisomya892@gmail.com>
2024-02-21 18:17:36 +0530

    I have complete my task

---
+++ SomyaTyagiji/Day5/01.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ int main(){
+ int num, i,sum = 0;
+
+ printf("Enter the number:");
+ scanf("%d",&num);
+ if(num == 0){
+    printf("0 is not a perfect number");
+    return 0;
+ }
+
+ for (i =1;i<num;i++){
+if(num % i == 0 )
+{   sum += i;}
+
+ }
+if (sum == num){
+printf("%d is a perfect number",num);}
+else{
+printf("%d is not a perfect number",num);}
+return 0;}
+
+
+
+
