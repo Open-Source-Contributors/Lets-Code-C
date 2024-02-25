b4b959bc48ad7a831b6cf5bb2c85ab36daa07846
Raj gupta <153410891+Sraj1724@users.noreply.github.com>
2024-02-25 14:05:46 +0530

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+struct station{
+    int station id;
+    char station name[50];
+    char arrivaltime[10];
+};
+void display timetable(struct station timetable)
+printf("train timetable\n")
+int main
+{
+
+     struct station timetable
+    {
+        (001, Tidmouth sheds,08:00am),
+        (002,knapford,08:30am),
+        (003,ffarquhar,09:15am),
+        (004,wellsworth,10:00am),
+        (005,brendam docks,11:30am),
+        (006,vicarstown,01:00pm),
+        (007,steamworks,03:00pm),
+    };
+    void search by id
+    int id,
+    for(int i = 0; i < 7;i++){
+        if(timetable.station id =id)
+        printf("station found");
+    }
+    void update arrival time
+    int id,
+    char time,
+    for (int i =0;i,7;i++){
+        if (timetable.stationid=id)
+        strcpy(timetable.arrivaltime);
+    printf("arrival time updated")
+    }
+}
