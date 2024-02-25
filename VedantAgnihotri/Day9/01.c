398d319b316f421cc59274c99eeb3f6d84afca1b
vedantagnihptri2004 <vedantagnihotri009@gmail.com>
2024-02-25 11:57:49 +0530

    updated

--- VedantAgnihotri/Day9/01.c
+++ VedantAgnihotri/Day9/01.c
@@ -3,47 +3,54 @@
 
 struct Station {
     int stationID;
-    char stationName[50];
-    char arrivalTime[10];
+    char stationname[50];
+    char arrivaltime[10];
 };
 
-// Function prototypes
-void createTimetable(struct Station timetable[]);
-void displayTimetable(struct Station timetable[]);
-void searchByID(struct Station timetable[], int id);
-void updateArrivalTime(struct Station timetable[], int id, char newTime[]);
+// decleration of function prototypes hadbeen applied here
+void createtimetable(struct Station timetable[]);
+void displaytimetable(struct Station timetable[]);
+void searchbyID(struct Station timetable[], int id);
+void updatearrivaltime(struct Station timetable[], int id, char newtime[]);
 
 int main() {
     struct Station timetable[7]; 
 
-    // Creating  timetable
-    createTimetable(timetable);
+    // Creating timetable of train
+    createtimetable(timetable);
 
-    // Display timetable
-    printf("Train Timetable:\n");
-    displayTimetable(timetable);
+    // Displaying the timetable
+    printf("Timetable for Train :\n");
+    displaytimetable(timetable);
 
     // Search by ID
-    int searchID = 004; 
-    printf("\nSearching for station with ID %d:\n", searchID);
-    searchByID(timetable, searchID);
+    int decleareID; 
+    printf("\nPlease enter the unique ID of the station you are searching for:\n");
+    scanf("%d", &decleareID);
+    searchbyID(timetable, decleareID);
 
     // Update the arrival time of thomas the tank engine
-    int updateID = 005; 
-    char newTime[] = "10:30 AM"; 
-    printf("\nUpdating arrival time for station with ID %d to %s:\n", updateID, newTime);
-    updateArrivalTime(timetable, updateID, newTime);
+    int updateID;
+    char newtime[20]; 
+    printf("\nPlease enter the unique ID of the station you want to update the arrival time for:\n");
+    scanf("%d", &updateID);
+    printf("\nPlease enter the new arrival time:\n");
+    scanf("%s", newtime);
+    printf("\nUpdate Time of Arrival for station with it's unique train ID %d to %s:\n", updateID, newtime);
+    updatearrivaltime(timetable, updateID, newtime);
 
     // Display updated timetable
     printf("\nUpdated Train Timetable:\n");
-    displayTimetable(timetable);
+    displaytimetable(timetable);
 
     return 0;
 }
 
-// Function to create the train timetable
-void createTimetable(struct Station timetable[]) {
-    struct Station stations[7] = {
+// Function to create the train timetable is defined here
+void createtimetable(struct Station timetable[])
+ {
+    struct Station stations[7] =
+     {
         {001, "Tidmouth Sheds", "08:00 AM"},
         {002, "Knapford", "08:30 AM"},
         {003, "Ffarquhar", "09:15 AM"},
@@ -53,37 +60,39 @@ void createTimetable(struct Station timetable[]) {
         {007, "Steamworks", "03:00 PM"}
     };
 
-    memcpy(timetable, stations, sizeof(stations));
+    memmove(timetable, stations, sizeof(stations));
 }
 
 // Function to display the entire time table
-void displayTimetable(struct Station timetable[]) {
-    for (int i = 0; i < 7; i++) {
-        printf("Station ID: %03d | Name: %16s | Arrival Time: %s\n", timetable[i].stationID, timetable[i].stationName, timetable[i].arrivalTime);
+void displaytimetable(struct Station timetable[]) {
+    for (int i = 0; i < 7; i++)
+     {
+        printf("Station ID: %03d | Name: %16s | Arrival Time: %s\n", timetable[i].stationID, timetable[i].stationname, timetable[i].arrivaltime);
     }
 }
 
 // Function to search for a station by it's ID
-void searchByID(struct Station timetable[], int id) {
-    for (int i = 0; i < 7; i++) {
-        if (timetable[i].stationID == id) {
+void searchbyID(struct Station timetable[], int id) {
+    for (int i = 0; i < 7; i++)
+     {
+        if (timetable[i].stationID == id)
+         {
             printf("Station found:\n");
-            printf("Station ID: %03d | Name: %16s | Arrival Time: %s\n", timetable[i].stationID, timetable[i].stationName, timetable[i].arrivalTime);
+            printf("Station ID: %03d | Name: %16s | Arrival Time: %s\n", timetable[i].stationID, timetable[i].stationname, timetable[i].arrivaltime);
             return;
         }
     }
-    
 }
 
 // Function to update the arrival time of a specific station
-void updateArrivalTime(struct Station timetable[], int id, char newTime[]) {
+void updatearrivaltime(struct Station timetable[], int id, char newtime[]) {
     for (int i = 0; i < 7; i++) {
-        if (timetable[i].stationID == id) {
-            strcpy(timetable[i].arrivalTime, newTime);
+        if (timetable[i].stationID == id)
+         {
+
+            strcpy(timetable[i].arrivaltime, newtime);
             printf("The arrivaltime of train had upated sucessfully, sorry for the inconvenience caused.\n");
-            return 0;
+            return;
         }
     }
-   
 }
-
