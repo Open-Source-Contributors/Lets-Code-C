e14011de8dce0e1b69fb620d981bb5a5fbd474ac
mahak121 <mahakpal2406@gmail.com>
2024-02-27 22:48:56 -0300

    Description of your changes

--- MahakPal/Day11/01.c
+++ MahakPal/Day11/01.c
@@ -3,13 +3,16 @@
 int main() {
     FILE *file = fopen("harry_potter.txt", "w");
 
-    
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;  
+    }
      char *First_Paragraph = ("As Harry Potter nervously approached the front of the Great Hall during the Sorting Hat ceremony, he couldn't help but feel a mix of anticipation and anxiety. The entire school waited in silence as the magical hat was placed upon his head. In that moment, the Sorting Hat delved into the depths of Harry's mind, considering his qualities and potential destinies. The Hat's voice echoed through the hall, 'Difficult, very difficult... where to put you? Ah, I see bravery, courage, and a thirst to prove yourself. Better be GRYFFINDOR!' The cheers erupted, and Harry felt a sense of belonging as he joined the table of the brave and courageous Gryffindor students.");
-     printf("As Harry Potter nervously approached the front of the Great Hall during the Sorting Hat ceremony, he couldn't help but feel a mix of anticipation and anxiety. The entire school waited in silence as the magical hat was placed upon his head. In that moment, the Sorting Hat delved into the depths of Harry's mind, considering his qualities and potential destinies. The Hat's voice echoed through the hall, 'Difficult, very difficult... where to put you? Ah, I see bravery, courage, and a thirst to prove yourself. Better be GRYFFINDOR!' The cheers erupted, and Harry felt a sense of belonging as he joined the table of the brave and courageous Gryffindor students/n", First_Paragraph);
+     fprintf(file, "%s", First_Paragraph);
+    fputs("\n", file);
 
-   
   char *Second_Paragraph = ("As Harry settled into his newfound home at Gryffindor, he formed lifelong friendships and faced countless adventures. The Sorting Hat's decision marked the beginning of a remarkable journey, where Harry discovered the true meaning of loyalty, bravery, and the strength that comes from the bonds of friendship. Little did he know that this choice would lead him to face the challenges that awaited him in the magical world and shape the destiny of the wizarding realm.");
-  printf("As Harry settled into his newfound home at Gryffindor, he formed lifelong friendships and faced countless adventures. The Sorting Hat's decision marked the beginning of a remarkable journey, where Harry discovered the true meaning of loyalty, bravery, and the strength that comes from the bonds of friendship. Little did he know that this choice would lead him to face the challenges that awaited him in the magical world and shape the destiny of the wizarding realm.\n", Second_Paragraph);
+ fprintf(file, "%s", Second_Paragraph);
 
 fclose(file);
 return 0;
