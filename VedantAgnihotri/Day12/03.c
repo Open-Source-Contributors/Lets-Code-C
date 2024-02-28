b229a60e7fb923994392749fd0d41266a0b386dd
vedantagnihptri2004 <vedantagnihotri009@gmail.com>
2024-02-28 22:21:44 +0530

    here are changesyou mentioned

--- VedantAgnihotri/Day12/03.c
+++ VedantAgnihotri/Day12/03.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Enter an odd positive integer between 1 and 6: ");
+    int i, j, n;
+    
+    printf("Enter an odd positive integer between 1 to 6 : ");
     scanf("%d", &n);
-
-    while(n < 1 || n > 6 || n % 2 == 0) {
-        printf("Invalid input. Please enter an odd positive integer between 1 and 6: ");
-        scanf("%d", &n);
+    
+    if (n % 2 != 1 || n < 1 || n > 6) {
+        printf("Please enter non negative positive integer between 1 and 6.\n");
+        return 1; 
     }
 
-    if(n % 3 == 0) {
-        printf("***\n");
-        printf(" *\n");
-        printf(" *\n");
-        printf("***\n");
-        printf("***\n");
-         printf(" *\n");
-    }
-     if(n % 5 == 0) {
-        printf("*****\n");
-        printf(" *\n");
-        printf(" *\n");
-        printf("*****\n");
-        printf("*****\n");
-         printf(" *\n");
+for (int i = n / 2; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {
+            if (j >= n/2 - i && j <= n / 2 + i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
     }
 
+for(i=0; i<n/2+1; i++) {
+        for(j=0; j<n; j++) {
+            if(j >= n/2-i && j <= n/2+i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    
+    for (int i = n / 2; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {
+            if (j >= n/2 - i && j <= n / 2 + i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
 
 
-    return 0;
+return 0;
 }
