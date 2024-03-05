c3f3302c455b19f3b6e1fab0cd7da9e6ac823c97
Somya Tyagi <tyagisomya892@gmail.com>
2024-03-05 19:18:19 +0530

    I am unable to understand the question

---
+++ SomyaTyagiji/Day17/01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ #define Building 3
+ #define floor 10
+ #define room 20
+ int hotelarray[3][10][20] = {A,B,C}{1,2,3,4,5,6,7,8,9,,10}{1,2,3, .... , 20};
+ printf("enter the building,floor,room number\n");
+ //room in building A
+ printf("floor 1").......
+
+
+
+
+
+
+}
